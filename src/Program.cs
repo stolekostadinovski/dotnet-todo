d5b1f192b33e6e32f1493427b7c9eb7cb31a357f
Stole Kostadinovski <83335822+stolekostadinovski@users.noreply.github.com>
2024-04-02 19:18:02 +0200

    MINOR

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 // <snippet_all>
 using Microsoft.EntityFrameworkCore;
-
+// var
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
